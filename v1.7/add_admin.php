445cb7bce73196b1f44de0aed619f2964ad46aff
karollubelski <karollubelski21@wp.pl>
2024-12-08 19:07:59 +0100

    Renamed folders for better organization

---
+++ v1.7/add_admin.php
@@ -0,0 +1,32 @@
+<?php
+// Połączenie z bazą danych
+$host = 'localhost';
+$dbname = 'moja_strona';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Błąd połączenia z bazą danych: " . $e->getMessage());
+}
+
+$login = 'admin123';
+$password = 'admin123';
+$email = 'tmail4230@gmail.com';
+
+// ja dodaje bez hashowania bo jest łatwiej wyciągnąć do maila te hasło
+// $hashed_password = password_hash($password, PASSWORD_DEFAULT); // hashowanie
+
+$sql = "INSERT INTO administrators (login, password, email) VALUES (:login, :password, :email)";
+$stmt = $pdo->prepare($sql);
+
+$stmt->execute([
+    ':login' => $login,
+    ':password' => $password,
+    ':email' => $email
+]);
+
+echo "Administrator został dodany pomyślnie!";
+?>
